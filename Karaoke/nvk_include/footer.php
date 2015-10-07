9654f8d756468f22e95f26efbda8fa2e49a9dcc4
kuem2405 <nguyenvankha2405@gmail.com>
2015-10-07 21:52:54 +0700

    up date

--- Karaoke/nvk_include/footer.php
+++
@@ -1,9 +0,0 @@
-</fieldset>
-		</div>
-		<div id="footer">
-			<h1><a href="#"><img src="nvk_images/logo.ico">KARAOKE NNICE</a></h1>
-			<h2>Design by kuem2405</h2>
-		
-		</div>
-	</body>
-</html>
--- Karaoke/thoat.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_destroy();
-    // delete cookies
-    setcookie('PHPSESSID', '', time()-3600, '/','', 0, 0);
-    header("Location: http://http://kuem2405.esy.es"); 
-?>
